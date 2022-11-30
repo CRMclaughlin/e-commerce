27219a0f8bf18caf72f5bc5e6fd163fb561fb0bf
CRMclaughlin <chrisraymusik@gmail.com>
2022-11-29 20:47:39 -0600

    finished product-routes.js, not sure about create & update

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const allProducts = await Product.findAll({
+      include: [{model: Category}, {model: Tag}]
+    })
+    res.status(200).json(allProducts)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const oneProduct = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}]
+    })
+    if (!oneProduct) {
+      res.status(404).json({message: "No product with this id!"})
+      return
+    }
+    res.status(200).json(oneProduct)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -89,8 +105,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(deleteProduct)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
