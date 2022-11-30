5a8843233dfe8595c8a2b08b2ade65724d3808f5
CRMclaughlin <chrisraymusik@gmail.com>
2022-11-29 21:10:21 -0600

    added get routes to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const allTags = await Tag.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(allTags)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+router.get('/:id', async (req, res) => {
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    if (!singleTag) {
+      res.status(404).json({ message: "No tag with this ID!"})
+      return
+    }
+    res.status(200).json(singleTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
